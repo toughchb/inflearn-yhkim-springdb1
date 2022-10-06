893da30001327e14918beb31d2c8e34d875809c2
toughchb <ggang2000@gmail.com>
2022-10-06 23:11:37 +0900

    스프링과 문제 해결 - 트랜잭션 까지

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,18 @@
+package hello.jdbc.connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        Assertions.assertThat(connection).isNotNull();
+    }
+}
